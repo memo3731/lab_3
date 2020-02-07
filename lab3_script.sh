7364946c939abb2d67486dd8e83318f65449c46b
Megan Moss <meganmoss@engr2-1-17-166-edu.int.colorado.edu>
2020-02-07 11:24:47 -0700

    Adding all of the files for lab 3

---
+++ lab3_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Authors: Megan Moss and Anne Chen
+# Date: 2/7/2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+echo "Enter a file name:"
+read fileName
+echo "Enter a regular expression:"
+read regExp
+grep "$regExp" "$fileName"
+grep -c - "regex_practice.txt" 
+grep -c @ "regex_practice.txt" 
+grep -o 303[0-9]{7} "regex_practice.txt" 
+grep .*@geocities.com "regex_practice.txt" >> "email_results.txt" 
+git add email_results.txt
+git commit -m "committed" 
